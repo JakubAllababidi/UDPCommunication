f84fbf1f1d7e7bb0a976b0aafa93e8a6d3204450
JakubAllababidi <jakuballababidi@gmail.com>
2025-03-07 11:33:33 +0100

    Aggiunti metodi

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,47 @@
-import java.net.DatagramSocket;
+import java.net.*;
 
 public class Client {
-    public DatagramSocket socket;
+    String nome;
+    int porta;
+    DatagramSocket dSocket;
+    DatagramPacket outPacket;
+    DatagramPacket inPacket;
+    InetAddress serverAddress; //indirizzo ip
 
-   public Client (int socket){
+    public Client(String nome, int porta) {
+        this.nome = nome;
+        this.porta = porta;
 
-   }
+    }
+   
 
+    public void inizio() {
+        try {
+            serverAddress = InetAddress.getLocalHost();
+            dSocket = new DatagramSocket();
+            System.out.println("DatagramSocket  del Client creato!");
+        } catch (SocketException e) {
+            System.err.println("Errore durante la creazione del DatagramSocket");
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            System.err.println("Errore ricerca dell'indirizzo del Server.");
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void richiedi() {
+    }
+
+    public void ricevi() {
+    }
+
+    public void chiudi() {
+        if (dSocket != null) {
+            dSocket.close();
+            System.out.println("Comunicazione interrotta");
+        }
+
+    }
 }
+
+
