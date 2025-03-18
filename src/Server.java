faf22b52fd6b68defad1d7b7ad123d55b599931e
JakubAllababidi <jakuballababidi@gmail.com>
2025-03-18 11:39:33 +0100

    aggiunto server() e ascolto ()

--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,38 @@
+import java.io.IOException;
+import java.net.BindException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.Date;
+
 public class Server {
+
+    public int port = 3200;
+
+    public DatagramSocket dSocket;
+    public DatagramPacket inPacket;
+    public DatagramPacket outPacket;
+    public byte[] bufferIn, bufferOut;
+    public String messageIn, messageOut;
+    public Date d;
+
+    public Server() {
+        try {
+            // Creazione del socket UDP sulla porta specificata
+            dSocket = new DatagramSocket(port);
+            System.out.println("Server avviato sulla porta: " + port);
+        } catch (BindException e) {
+            System.err.println("Errore: porta già in uso.");
+        } catch (SocketException e) {
+            System.err.println("Errore nella creazione del socket UDP.");
+        }
+    }
+
+    public void ascolto() {
+        while (true) {
+            System.out.println("Server in ascolto sulla porta " + port + "!\n");
+            bufferIn = new byte[256];
+        }
+    }
 }
